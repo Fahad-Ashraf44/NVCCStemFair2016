1109876560effba017c7207d635c671a9642af17
Jacob <jacobboisseau@gmail.com>
2016-03-19 21:47:43 -0400

    Added B/W Gmaps

--- MapSrc.js
+++ MapSrc.js
@@ -1,12 +1,91 @@
-function initialize()
-{
-  var mapProp =
-      {
-          center:new google.maps.LatLng(38.904722,-77.016389),
-          zoom:10,
-          mapTypeId:google.maps.MapTypeId.ROADMAP
-      };
-  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
+        var map;
+        var washington = new google.maps.LatLng(38.904722,-77.016389);
 
-google.maps.event.addDomListener(window, 'load', initialize);
+        function initialize() {
+
+            var roadAtlasStyles = [
+  {
+       "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        { "saturation": -100 },
+        { "lightness": -8 },
+        { "gamma": 1.18 }
+      ]
+  }, {
+      "featureType": "road.arterial",
+      "elementType": "geometry",
+      "stylers": [
+        { "saturation": -100 },
+        { "gamma": 1 },
+        { "lightness": -24 }
+      ]
+  }, {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [
+        { "saturation": -100 }
+      ]
+  }, {
+      "featureType": "administrative",
+      "stylers": [
+        { "saturation": -100 }
+      ]
+  }, {
+      "featureType": "transit",
+      "stylers": [
+        { "saturation": -100 }
+      ]
+  }, {
+      "featureType": "water",
+      "elementType": "geometry.fill",
+      "stylers": [
+        { "saturation": -100 }
+      ]
+  }, {
+      "featureType": "road",
+      "stylers": [
+        { "saturation": -100 }
+      ]
+  }, {
+      "featureType": "administrative",
+      "stylers": [
+        { "saturation": -100 }
+      ]
+  }, {
+      "featureType": "landscape",
+      "stylers": [
+        { "saturation": -100 }
+      ]
+  }, {
+      "featureType": "poi",
+      "stylers": [
+        { "saturation": -100 }
+      ]
+  }, {
+  }
+            ]
+
+            var mapOptions = {
+                zoom: 10,
+                center: washington,
+                mapTypeControlOptions: {
+                    mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'usroadatlas']
+                }
+            };
+
+            map = new google.maps.Map(document.getElementById('googleMap'),
+                mapOptions);
+
+            var styledMapOptions = {
+
+            };
+
+            var usRoadMapType = new google.maps.StyledMapType(
+                roadAtlasStyles, styledMapOptions);
+
+            map.mapTypes.set('usroadatlas', usRoadMapType);
+            map.setMapTypeId('usroadatlas');
+        }
+
+       google.maps.event.addDomListener(window, 'load', initialize);
