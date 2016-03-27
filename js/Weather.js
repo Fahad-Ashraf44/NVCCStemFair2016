2a7b0dc7ff1d5f3d3112b1935cb5d0c0e4184294
Fahad Ashraf <fahadfx@gmail.com>
2016-03-27 01:45:42 -0400

    Added a few css modifications that I thought looked a little better than plain black. Still a work in progress. Also trying to remove all API keys to mitigate security threats which means anyone else using the code needs to get their own keys. Still working on the weather api

---
+++ js/Weather.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById('weather').innerHTML = "weather";
+
+//trying to get this to print on the webpage but haven't been able to figure it out yet
+//please chime in if you think you might know how to make it work!
