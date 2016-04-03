c5e74d5728c437862d6f5218e2ec9f88e415a9b1
Jacob <jacobboisseau@gmail.com>
2016-04-02 21:43:00 -0400

    Made weather work...Finally!!!

--- js/Weather.js
+++ js/Weather.js
@@ -1,6 +1,10 @@
 
+window.onload = function()
+{
+    document.getElementById('weather').innerHTML = 'weather';
+}
 
-document.getElementById('weather').innerHTML = "weather";
+
 
 //trying to get this to print on the webpage but haven't been able to figure it out yet
 //please chime in if you think you might know how to make it work!
