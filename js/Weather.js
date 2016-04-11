b62bd227e3bdbb18335a1d584831470317fa6629
Fahad Ashraf <fahadfx@gmail.com>
2016-04-10 22:32:09 -0400

    Added weather and  icons, getting close to being done!

--- js/Weather.js
+++ js/Weather.js
@@ -1,9 +1,35 @@
+jQuery(document).ready(function($) {
 
-window.onload = function()
-{
-    document.getElementById('weather').innerHTML = API.weatherKey;
-}
+  /* Edit these variables */
+  var api = API.weatherKey;
+  var state = "VA";
+  var city = "Annandale";
 
+  $.ajax({
+  url: "http://api.wunderground.com/api/" + api + "/forecast/conditions/q/" + state + "/" + city + ".json",
+  dataType : "jsonp",
+  success : function(parsed_json)
+      {
+	  var icon_json = '<img src ="src/Icons/Weather/Cloud.svg"/>';
+	  var temp_json = parsed_json['current_observation']['temp_f'];
+		temp_json += "<span>°F</span>";
+	  var condition_json = parsed_json['current_observation']['weather'];
+	  var real_feel_json = "Feels Like " + parsed_json['current_observation']['feelslike_f'] + "°F";
+	  var wind_json = 'Winds are ' + parsed_json['current_observation']['wind_string'];
+	  var location_json = city + ', ' + state;
+
+
+  document.getElementById("weather-icon").innerHTML = icon_json;
+  document.getElementById("temp").innerHTML = temp_json;
+  document.getElementById("condition").innerHTML = condition_json;
+  document.getElementById("real-feel").innerHTML = real_feel_json;
+  document.getElementById("wind").innerHTML = wind_json;
+  document.getElementById("location").innerHTML = location_json;
+
+
+  }
+  });
+});
 
 
 //trying to get this to print on the webpage but haven't been able to figure it out yet
